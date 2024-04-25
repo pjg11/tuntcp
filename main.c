3f26679289102ba83261484770f068b4e42a8ac4
Piya <45182159+pjg11@users.noreply.github.com>
2024-04-25 00:15:32 +0000

    icmp echo implementation + sending and receiving ping packet

--- main.c
+++ main.c
@@ -26,6 +26,76 @@ typedef struct {
   uint32_t dst;
 } ipv4;
 
+typedef struct {
+  uint8_t  type;
+  uint8_t  code;
+  uint16_t checksum;
+  uint16_t id;
+  uint16_t seq;
+} icmpecho;
+
+
+typedef struct {
+  union {
+    struct ping {
+      ipv4 ip;
+      icmpecho echo;
+    } ping;
+  };
+} packet;
+
+
+int openTun(char *dev) {
+  int fd, err;
+  struct ifreq ifr;
+
+  if((fd = open("/dev/net/tun", O_RDWR)) < 0)
+    return 1;
+
+  ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+  strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+  if((err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0) {
+    close(fd);
+    return err;
+  }
+  return fd;
+}
+
+// https://github.com/pandax381/microps/blob/ac3747f68a6fd590443d028b4eaf5d97c4c58e49/util.c#L38
+void hexdump(const void *data, size_t size) {
+  unsigned char *src;
+  int offset, index;
+
+  src = (unsigned char *)data;
+  for(offset = 0; offset < (int)size; offset += 16) {
+    printf("%08x ", offset);
+    for(index = 0; index < 16; index++) {
+      if ((offset + index) % 8 == 0)
+        printf(" ");
+
+      if(offset + index < (int)size) {
+        printf("%02x ", 0xff & src[offset + index]);
+      } else {
+        printf("   ");
+      }
+    }
+    printf(" |");
+    for(index = 0; index < 16; index++) {
+      if(offset + index < (int)size) {
+        if(isascii(src[offset + index]) && isprint(src[offset + index])) {
+            printf("%c", src[offset + index]);
+        } else {
+          printf(".");
+        }
+      } else {
+        printf(" ");
+      }
+    }
+    printf("|\n");
+  }
+}
+
 // https://www.rfc-editor.org/rfc/rfc1071#section-4.1
 uint16_t checksum(void *data, size_t count) {
 
@@ -65,61 +135,36 @@ ipv4 ip(size_t len_contents, uint8_t protocol, char *daddr) {
   return i;
 }
 
-// https://github.com/pandax381/microps/blob/ac3747f68a6fd590443d028b4eaf5d97c4c58e49/util.c#L38
-void hexdump(const void *data, size_t size) {
-  unsigned char *src;
-  int offset, index;
+icmpecho echo(uint16_t id, uint16_t seq) {
 
-  src = (unsigned char *)data;
-  for(offset = 0; offset < (int)size; offset += 16) {
-    printf("%08x ", offset);
-    for(index = 0; index < 16; index++) {
-      if ((offset + index) % 8 == 0)
-        printf(" ");
+  icmpecho e;
+  
+  e.type = 8;
+  e.code = 0;
+  e.checksum = 0;
+  e.id = htons(id);
+  e.seq = htons(seq);
 
-      if(offset + index < (int)size) {
-        printf("%02x ", 0xff & src[offset + index]);
-      } else {
-        printf("   ");
-      }
-    }
-    printf(" |");
-    for(index = 0; index < 16; index++) {
-      if(offset + index < (int)size) {
-        if(isascii(src[offset + index]) && isprint(src[offset + index])) {
-            printf("%c", src[offset + index]);
-        } else {
-          printf(".");
-        }
-      } else {
-        printf(" ");
-      }
-    }
-    printf("|\n");
-  }
-}
-
-int openTun(char *dev) {
-  int fd, err;
-  struct ifreq ifr;
-
-  if((fd = open("/dev/net/tun", O_RDWR)) < 0)
-    return 1;
-
-  ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
-  strncpy(ifr.ifr_name, dev, IFNAMSIZ);
-
-  if((err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0) {
-    close(fd);
-    return err;
-  }
-  return fd;
+  e.checksum = checksum(&e, sizeof(e));
+  return e;
 }
 
 int main(void) {
-  int tun = openTun("tun0");
-  ipv4 i = ip(0, PROTO_ICMP, "208.94.117.43");
-  hexdump(&i, sizeof(i));
+  int tun, bytes;
+  size_t size;
+  packet send, recv;
+
+  tun = openTun("tun0");
+
+  send.ping.echo = echo(12345, 1);
+  send.ping.ip = ip(sizeof(send.ping.echo), PROTO_ICMP, "8.8.8.8");
+  size = sizeof(send);
+
+  hexdump(&send, size);
+  write(tun, &send, size);
+  bytes = read(tun, &recv, size);
+  printf("\n");
+  hexdump(&recv, bytes);
 
   close(tun);
   return 0;
